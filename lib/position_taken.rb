6366cf46dea61fc7257c6341188729ceb79266d5
Micah Shute <micah.shute@gmail.com>
2018-02-03 23:20:16 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,4 @@
 def position_taken?(board, index)
-  puts board[index-1]
-  return !(board[index-1] == "" || board[index-1] == " " || board[index-1] == nil)
+  return !(board[index] == "" || board[index] == " " || board[index] == nil)
 end
 
